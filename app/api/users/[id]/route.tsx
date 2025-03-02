395562c1ecc5d294725da294421ca5283c190964
Ritesh Persaud <riteshpersaud2003@gmail.com>
2025-03-01 20:11:37 -0500

    Slight modification to user prisma endpoint

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -9,6 +9,8 @@ export async function GET(request: NextRequest, { params: { id } }: Props) {
   const user = await prisma.user.findUnique({
     where: { id: id },
   });
+  if (!user)
+    return NextResponse.json({ error: "Could not find user" }, { status: 404 });
 
   return NextResponse.json(user, { status: 200 });
 }
